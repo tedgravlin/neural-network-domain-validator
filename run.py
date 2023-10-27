403928da41a92a9c924c6d5d3e9a8bd19b678366
tedgravlin <theodoregravlin@pm.me>
2023-10-27 10:19:37 -0400

    Add more domains to the dataset

--- run.py
+++ run.py
@@ -7,23 +7,27 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 
 dataset = pd.read_csv('dataset.csv')
+print("Dataset read")
 
-ohe = OneHotEncoder()
-transformed = ohe.fit_transform(dataset[['Label']])
-print(transformed.toarray())
-print(ohe.categories_)
+#ohe = OneHotEncoder()
+#transformed = ohe.fit_transform(dataset[['Label']])
+#print(transformed.toarray())
+#print(ohe.categories_)
 
 y = dataset['Label']
 z = dataset['URL']
-z_train, z_test,y_train, y_test = train_test_split(z,y,test_size = 0.3)
+z_train, z_test,y_train, y_test = train_test_split(z,y,test_size = 0.2)
 
 cv = CountVectorizer()
 features = cv.fit_transform(z_train)
+print("fit_transform completed")
 
 model = svm.SVC()
 model.fit(features,y_train)
+print("model.fit completed")
 
 features_test = cv.transform(z_test)
+print("transform completed")
 
 predictions = model.predict(features_test)
 print('predicted', predictions)
