efe2c0f21397c0eb912416a24595bc2d51ecaed0
tedgravlin <theodoregravlin@pm.me>
2023-11-06 18:31:22 -0500

    Train with multiple features, not just one

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import joblib
+import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
-import joblib
-import os
+from sklearn.preprocessing import StandardScaler
+from scipy.sparse import hstack
 import matplotlib.pyplot as plot
 
 # FUNCTION: Train the model and store it locally
@@ -15,22 +17,31 @@ def create_model():
 
     # Turn the dataset into a pandas data frame
     dataframe = pd.DataFrame(dataset)
-
-    # Dataset columns
-    #features = ['Num Of Sections','TLD','TLD Length','Domain','Domain Length','URL']
-    #x = dataframe[features]
-    x = dataframe['URL']
+    x = dataframe[['Num Of Sections', 'TLD', 'TLD Length', 'Domain', 'Domain Length', 'URL']]
     y = dataframe['Label']
 
-    #Split train and test data
+    # Separate text and numeric features
+    text_features = ['TLD', 'Domain', 'URL']
+    numeric_features = ['Num Of Sections', 'TLD Length', 'Domain Length']
+
+    # Split training and test data
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
     # Initialize the TF-IDF vectorizer
     tfidf = TfidfVectorizer(min_df=1)
 
-    # Generate features for x_train and x_test
-    features_train = tfidf.fit_transform(x_train)
-    features_test = tfidf.transform(x_test)
+    # Turn the text columns into a string and vectorize
+    features_train_text = tfidf.fit_transform(x_train[text_features].apply(lambda row: ' '.join(row.astype(str)), axis=1))
+    features_test_text = tfidf.transform(x_test[text_features].apply(lambda row: ' '.join(row.astype(str)), axis=1))
+
+    # Scale the numeric features
+    scaler = StandardScaler()
+    features_train_numeric = scaler.fit_transform(x_train[numeric_features])
+    features_test_numeric = scaler.transform(x_test[numeric_features])
+
+    # Recombine the text_features and the numeric_features
+    features_train = hstack([features_train_text, features_train_numeric])
+    features_test = hstack([features_test_text, features_test_numeric])
 
     # MLP model
     model = MLPClassifier(
@@ -46,7 +57,7 @@ def create_model():
     # Fit the model
     model.fit(features_train, y_train)
 
-    # Predicting the Test set results
+    # Predicting the test set results
     predictions = model.predict(features_test)
 
     # Print the classification report
@@ -74,16 +85,33 @@ def create_model():
 
 # FUNCTION: Grab the model from storage and test it with some domains
 def test_model(model, tfidf):
-    # Test domains
-    test_domains = ['google.com','googl.com','facebook.com','google.nl', 'www.theverge.com', 'theverge.com']
+    # Get the test dataset CSV file
+    test_dataset = pd.read_csv("testdataset.csv")
+
+    # Turn the test dataset into a pandas data frame
+    dataframe = pd.DataFrame(test_dataset)
+    x = dataframe[['Num Of Sections', 'TLD', 'TLD Length', 'Domain', 'Domain Length', 'URL']]
+
+    # Separate text and numeric features
+    text_features = ['TLD', 'Domain', 'URL']
+    numeric_features = ['Num Of Sections', 'TLD Length', 'Domain Length']
+
+    # Turn the text columns into a string and vectorize
+    features_test_text = tfidf.transform(x[text_features].apply(lambda row: ' '.join(row.astype(str)), axis=1))
+
+    # Scale the numeric features
+    scaler = StandardScaler()
+    scaler.fit_transform(x[numeric_features])
+    features_test_numeric = scaler.transform(x[numeric_features])
+
+    # Recombine the text_features and the numeric_features
+    features_test = hstack([features_test_text, features_test_numeric])
 
     # Use the model to predict the label for each of 
     # the test domains and then print the result
-    for domain in range(len(test_domains)):
-        print("URL:", test_domains[domain])
-        test_result = tfidf.transform([test_domains[domain]])
-        prediction = model.predict(test_result)
-        print("PREDICTION:", prediction)  
+    print("URLS:", x['URL'].to_list())
+    prediction = model.predict(features_test)
+    print("PREDICTIONS:", prediction)  
 
 # FUNCTION: Delete the model from storage
 def erase_model():
