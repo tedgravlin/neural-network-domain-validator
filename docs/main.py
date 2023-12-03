34516d6c72f205caec21099b9098a0565cb190e2
tedgravlin <theodoregravlin@pm.me>
2023-12-02 19:14:37 -0500

    website: Only test the URL that the user entered
    Also:
    * Updated models
    * Light mode/dark mode

--- docs/main.py
+++ docs/main.py
@@ -5,7 +5,8 @@ from pyscript import display
 from sklearn.preprocessing import StandardScaler
 from scipy.sparse import hstack
 from pyweb import pydom
-from os.path import exists
+from urllib.parse import urlparse
+import csv
 
 # Get the input container element
 input_container = pydom['#input-container']
@@ -15,23 +16,39 @@ input_container.style["display"] = "block"
 progress_text = document.querySelector("#progress-text")
 progress_text.innerText = ""
 
+# Get the result text HTML element
+result_text = document.querySelector('#result-text')
+
 # Gets the model and returns it
 def load_files():
     model = joblib.load('model.pkl')
     tfidf = joblib.load('tfidf.pkl')
+
+    return model, tfidf
+
+def test_model(model, tfidf, formatted_url):
+    with open('testdataset.csv', mode ='w', newline='', encoding="utf-8") as test_file:
+        parameters = []
+        # Open writer to CSV file
+        csvWrite = csv.writer(test_file, delimiter=' ', escapechar=' ', quoting=csv.QUOTE_NONE)
+        # Add the labels to parameters
+        parameters.append('NumOfSections,TLD,TLDLength,Domain,DomainLength,URL')
+        # Write the labels to the CSV file
+        csvWrite.writerow(parameters)
+        parameters = []
+        # Add the URL to parameters
+        parameters.append(formatted_url)
+        csvWrite.writerow(parameters)
+
     test_dataset = pd.read_csv("testdataset.csv")
 
-    return model, tfidf, test_dataset
-
-def test_model(model, tfidf, test_dataset):
     # Turn the test dataset into a pandas data frame
     dataframe = pd.DataFrame(test_dataset)
-    x = dataframe[['Num Of Sections', 'TLD', 'TLD Length', 'Domain', 'Domain Length', 'URL']]
-    y = dataframe['Label']
+    x = dataframe[['NumOfSections', 'TLD', 'TLDLength', 'Domain', 'DomainLength', 'URL']]
 
     # Separate text and numeric features
     text_features = ['TLD', 'Domain', 'URL']
-    numeric_features = ['Num Of Sections', 'TLD Length', 'Domain Length']
+    numeric_features = ['NumOfSections', 'TLDLength', 'DomainLength']
 
     # Turn the text columns into a string and vectorize
     features_test_text = tfidf.transform(x[text_features].apply(lambda row: ' '.join(row.astype(str)), axis=1))
@@ -44,33 +61,41 @@ def test_model(model, tfidf, test_dataset):
     # Recombine the text_features and the numeric_features
     features_test = hstack([features_test_text, features_test_numeric])
 
-    # Use the model to predict the label for each of 
-    # the test domains and then print the result
-    display("URLS:", x['URL'].to_list())
+    # Use the model to predict the label for the url
     prediction = model.predict(features_test)
-    display("\nPREDIC:", prediction)  
-    count = 0
-    correct_count = 0
-    for label in range(len(y)):
-        if (prediction[label] == y.to_list()[label]):
-            correct_count = correct_count + 1
-        count = count + 1
-    display("ACTUAL:", y.to_list())
-    display("CORRECT PREDICTIONS:", correct_count,"/",count)
-    display("ACCURACY OF THIS TEST:", (correct_count/count) * 100)
+    result_text.innerText = "Prediction: " + prediction[0]
+    
 
 def get_url(event):
     input_text = document.querySelector("#url-input")
     url = input_text.value
-    output_text = document.querySelector("#result-text")
-    output_text.innerText = url
-    run(url)
+    run(get_formatted_url(url))
+
+def get_hostname(url):
+    if (urlparse(url).scheme == ""):
+        url = "https://" + url
+
+    result = urlparse(url)
+    return result.hostname
+
+def get_formatted_url(url):
+    link = get_hostname(url)
+    comma = ","
+    # Split the link into sections
+    linkSplit = link.split(".")
+    # Get the number of sections
+    numOfSections = len(linkSplit)
+    # Get the TLD and its length
+    tld = linkSplit[len(linkSplit) - 1]
+    tldLength = len(tld)
+    # Get the domain and its length
+    domain = linkSplit[len(linkSplit) - 2]
+    domainLength = len(domain)
+    return (str(numOfSections) + comma + tld + comma + str(tldLength) + comma + domain + comma + str(domainLength) + comma + link)
 
 def run(url):
-    # Load the model from storage
-    model, tfidf, test_dataset = load_files()
-    # Test the model with the test dataset
-    test_model(model, tfidf, test_dataset)
+    model, tfidf = load_files()
+    test_model(model, tfidf, url)
 
 
 
